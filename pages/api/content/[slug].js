7df6a043e44d6651cfd21b228ab453e4e8b4e8d4
Rohan <rhnmht30@gmail.com>
2021-05-14 16:36:43 +0530

    log: root directory

--- pages/api/content/[slug].js
+++ pages/api/content/[slug].js
@@ -16,6 +16,7 @@ export default (req, res) => {
 			.json({ message: "Please provide the type of content" });
 
 	try {
+		console.log(root);
 		const file = fs.readFileSync(
 			path.join(root, "data", type, `${slug}.mdx`),
 			"utf8"
--- pages/api/content/index.js
+++ pages/api/content/index.js
@@ -16,6 +16,7 @@ export default (req, res) => {
 			.json({ message: "Please provide the type of content" });
 
 	try {
+		console.log(root);
 		const files = fs.readdirSync(path.join(root, "data", type));
 		return res.status(200).json({ message: "success", files });
 	} catch (error) {
