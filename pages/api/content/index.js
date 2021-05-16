f81ed95ac649eb035c87a5c39117d9058da8e3f2
Rohan <rhnmht30@gmail.com>
2021-05-16 13:52:41 +0530

    fix: provide list of all files when no type is given

--- pages/api/content/index.js
+++ pages/api/content/index.js
@@ -3,20 +3,41 @@ import path from "path";
 
 const root = process.cwd();
 
-export default (req, res) => {
+const getFiles = async (loc) => {
+	const entries = fs.readdirSync(loc, { withFileTypes: true });
+
+	const files = entries
+		.filter((file) => !file.isDirectory())
+		.map((file) => ({
+			...file,
+			name: file.name.replace(".mdx", ""),
+			type: loc.split("/data/")[1].slice(0, -1),
+		}));
+
+	const folders = entries.filter((folder) => folder.isDirectory());
+
+	for (const folder of folders)
+		files.push(...(await getFiles(`${loc}/${folder.name}/`)));
+
+	return files;
+};
+
+export default async (req, res) => {
 	if (req.method !== "GET")
 		return res
 			.status(405)
 			.json({ message: `${req.method} method not allowed` });
 
 	const { type } = req.query;
-	if (!type)
-		return res
-			.status(400)
-			.json({ message: "Please provide the type of content" });
 
 	try {
-		const files = fs.readdirSync(path.join(root, "data", type));
+		let files;
+		if (type) {
+			files = fs.readdirSync(path.join(root, "data", type));
+		} else {
+			files = await getFiles(path.join(root, "data"));
+		}
+
 		return res.status(200).json({ message: "success", files });
 	} catch (error) {
 		return res.status(404).json({
